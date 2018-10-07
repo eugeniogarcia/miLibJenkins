84501d4ec9586372ea5353b68e12fe061f9fa28b
Eugenio <egsmartin@hotmail.com>
2018-10-07 17:53:56 +0200

    Segundo borrador

--- src/org/euge/Operaciones.groovy
+++ src/org/euge/Operaciones.groovy
@@ -2,11 +2,6 @@ package org.euge;
 import org.euge.Point;
 
 class Operaciones {
-	private Vector ref;
-	
-	Operaciones (Point ref){
-		this.ref=ref;
-	}
 	
 	public Vector resta(Point A, Point B){
 		return new Vector(A.x-B.x,A.y-B.y,A.z-B.z);
@@ -18,10 +13,6 @@ class Operaciones {
 	
 	public Vector multiplica(float c, Point A){
 		return new Vector(A.x*c,A.y*c,A.z*c);
-	}	
-	
-	public Vector absoluto(Point A){
-		return suma(A,ref);
 	}	
 	
 }
